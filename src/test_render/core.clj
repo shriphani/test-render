d6c66fe1b0e91a28237d6c457ac63ee37d072a19
Shriphani Palakodety <shriphanip@gmail.com>
2016-04-13 17:53:53 -0700

    added render output

--- src/test_render/core.clj
+++ src/test_render/core.clj
@@ -1,6 +1,19 @@
-(ns test-render.core)
+(ns test-render.core
+  (:require [clojure.java.io :as io])
+  (:import [org.openqa.selenium WebDriver OutputType]
+           [com.machinepublishers.jbrowserdriver Timezone JBrowserDriver Settings]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
-  (println x "Hello, World!"))
+  (let [settings (-> (Settings/builder)
+                     (.timezone Timezone/AMERICA_NEWYORK)
+                     (.build))
+
+        driver   (JBrowserDriver. settings)
+
+        dest-file "screenshot.png"]
+    (.get driver "http://blog.shriphani.com")
+    (let [f (.getScreenshotAs driver OutputType/FILE)]
+      (io/copy f
+               (io/file dest-file)))))
